11023723b7dd5bbc9d326f15350041cb89c81c74
Amine <amine.benzaggagh@icloud.com>
2024-09-16 12:31:05 +0200

    Directive Attribut

---
+++ src/app/directive/scroll-to.directive.ts
@@ -0,0 +1,23 @@
+import {Directive, HostListener, Input} from '@angular/core';
+
+@Directive({
+  selector: '[appScrollTo]',
+  standalone: true
+})
+export class ScrollToDirective {
+
+  @Input()
+  target = '';
+
+  constructor() { }
+
+  @HostListener('click')
+  onClick() {
+    const targetElement = document.querySelector(this.target);
+    if (!targetElement) {
+      throw new Error(`target is required.`);
+    }
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+}
---
+++ src/app/directive/show-for.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appShowFor]',
+  standalone: true
+})
+export class ShowForDirective {
+
+  constructor() { }
+
+}
